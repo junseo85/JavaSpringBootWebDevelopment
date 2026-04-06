fb29e594ac45f347054731dd93ff03effe40f651
Jun Seo <112793862+junseo85@users.noreply.github.com>
2026-04-06 01:39:46 -0500

    web page using thymeleaf for orders, Search, Add and Admin page. Nav bar is updated with working link as well. admin page includes working delete and update button function

--- src/main/java/com/junauto/practicespringlogin/controllers/OrdersController.java
+++ src/main/java/com/junauto/practicespringlogin/controllers/OrdersController.java
@@ -1,16 +1,16 @@
 package com.junauto.practicespringlogin.controllers;
 
 import com.junauto.practicespringlogin.models.OrderModel;
+import com.junauto.practicespringlogin.models.SearchModel;
 import com.junauto.practicespringlogin.services.OrderBusinessService;
 
 import com.junauto.practicespringlogin.services.OrdersBusinessServiceInterface;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +44,84 @@ public class OrdersController {
         return  "orders";
     }
 
+    @GetMapping("/showNewOrderForm")
+    public String showNewFOrm(Model model){
+        model.addAttribute("order",new OrderModel());
+        return "addNewOrderForm.html";
+    }
+
+    @PostMapping("/addNew")
+    public String addnew(@Valid OrderModel newOrder, BindingResult bindingResult, Model model){
+        newOrder.setId(null);
+        //add to the database
+
+        service.addOne(newOrder);
+        //get all orders from database
+        List<OrderModel> orders = service.getOrders();
+        //show all orders page.
+        model.addAttribute("orders", orders);
+        return "orders";
+    }
+
+    @GetMapping("/showSearchForm")
+    public String showSearchForm(Model model){
+        model.addAttribute("searchModel",new SearchModel());
+        return "searchForm.html";
+    }
+
+    @PostMapping("/search")
+    public String search(@Valid SearchModel searchModel, BindingResult bindingResult, Model model){
+        String searchTerm = searchModel.getSearchTerm();
+        List<OrderModel> orders = service.searchOrders(searchTerm);
+        model.addAttribute("orders", orders);
+        return "orders";
+    }
+
+    @GetMapping("/admin")
+    public String showAdminPage(Model model){
+        //generate some orders
+        //this version of the app will send a hard-coded list of orders
+
+
+        List<OrderModel> orders = service.getOrders();
+
+
+        model.addAttribute("title","Here is what I want to do this summer");
+        model.addAttribute("orders", orders);
+        return  "ordersAdmin.html";
+    }
+
+    @PostMapping("/editForm")
+    public String displayEditForm(OrderModel orderModel, Model model){
+        //Display new order form
+        model.addAttribute("title", "Edit order");
+        model.addAttribute("orderModel", orderModel);
+        return "editForm.html";
+    }
+
+    @PostMapping("/doUpdate")
+    //process a request from the AddOrderForm. Add a new order to the database. Show all orders.
+    public String updateOrder(@Valid OrderModel order, BindingResult bindingResult, Model model){
+        //add the new order
+        service.updateOne(order.getId(), order);
+        //get updated list of all the orders
+        List<OrderModel> orders = service.getOrders();
+
+        //display all orders
+        model.addAttribute("orders", orders);
+
+        return "ordersAdmin.html";
+    }
+
+    @PostMapping("/delete/")
+    public String deleteOrder(@Valid OrderModel order, BindingResult bindingResult, Model model){
+        service.deleteOne(order.getId());
+        //get updated list of all the orders
+        List<OrderModel> orders = service.getOrders();
+        //display all orders
+        model.addAttribute("orders", orders);
+        return "ordersAdmin.html";
+    }
 
     @GetMapping("/test")
     public String test(){
---
+++ src/main/java/com/junauto/practicespringlogin/models/SearchModel.java
@@ -0,0 +1,16 @@
+package com.junauto.practicespringlogin.models;
+
+public class SearchModel {
+    String searchTerm;
+
+    public SearchModel() {
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+}
